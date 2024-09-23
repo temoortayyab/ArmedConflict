c2b7ba9188ffd52743f7d453a2fe98d40672ac15
temoortayyab <temoortayyab@gmail.com>
2024-09-23 17:26:38 -0400

    Read in new data.

--- R/merged_dataset.R
+++ R/merged_dataset.R
@@ -9,19 +9,37 @@ covariates <- read.csv(here("data", "Original", "covariates.csv"), header = TRUE
 covariates <- covariates %>%
   rename(Year = year)
 
-#merging datasets based on year and ISO
-merged_data <- data4 %>% #disaster data
-  full_join(new_combined, by = c("ISO", "Year")) %>% #mortality data
-  full_join(conflict1, by = c("ISO", "Year")) %>% #conflict data
-  full_join(covariates, by = c("ISO", "Year"))
+#putting all data frames into list
+alllist <- list(new_combined, conflict1, data4)
+
+#merging all data frames in list
+alllist |> reduce(full_join, by = c('ISO', 'Year')) -> finaldata0
+
+finaldata <- covariates |>
+  left_join(finaldata0, by = c('ISO', 'Year'))
+
+#checking where values are NA
+na_counts <- sapply(finaldata, function(x) sum(is.na(x)))
+
+# Display columns with NA values
+na_columns <- na_counts[na_counts > 0]
+print("Columns with NA values:")
+print(na_columns)
+
+# need to fill in NAs with 0's for conflict, drought, earthquake
+finaldata <- finaldata |>
+  mutate(conflict = replace_na(conflict, 0),
+         Drought = replace_na(Drought, 0),
+         Earthquake = replace_na(Earthquake, 0))
 
 #counting if there are 20 rows for each country
-iso_counts <- merged_data %>%
+iso_counts <- finaldata %>%
   group_by(ISO) %>%
   summarise(count = n(), .groups = 'drop')
 
+#checking if there are any countries with less or more than 20 rows
 countries_other_than_20 <- iso_counts %>%
   filter(count != 20)
-print(countries_other_than_20) #there are some
+print(countries_other_than_20) #None
 
-write.csv(merged_data, here("data", "merged_data.csv"), row.names = FALSE)
+write.csv(finaldata, file = here("data", "analytical", "finaldata.csv"), row.names = FALSE)
